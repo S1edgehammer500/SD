fe6970a03e27d3757473cac9fe13f54caf577485
S1edgehammer5000 <151660385+S1edgehammer5000@users.noreply.github.com>
2024-04-10 21:49:28 +0100

    Fixed foreign keys

--- SD/Model/Database.py
+++ SD/Model/Database.py
@@ -1,8 +1,7 @@
-
 import sqlite3
 
 def openConnection():
     conn = sqlite3.connect('HorizonRestaurantDB.db')
+    conn.execute("PRAGMA foreign_keys = ON;")
     cur = conn.cursor()
-    
     return conn, cur
